3459a0cca0975b153b97f45fbf352205920435bb
Hris <xrisko06@abv.bg>
2025-10-18 13:54:01 +0300

    Add task

---
+++ Week 02/Find The Difference/FindTheDifferenceCounter.cpp
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/find-the-difference/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+
+	// Space O(1)
+	// Time O(s + t)
+	char findTheDifference(string s, string t) {
+		const size_t LETTERS_COUNT = 26;
+		int sCounter[LETTERS_COUNT] = {};
+		int tCounter[LETTERS_COUNT] = {};
+
+		for (char c : s)
+			sCounter[c - 'a']++;
+
+		for (char c : t)
+			tCounter[c - 'a']++;
+
+		for (size_t i = 0; i < LETTERS_COUNT; i++)
+		{
+			if (sCounter[i] != tCounter[i])
+			{
+				return i + 'a';
+			}
+		}
+
+		return '?';
+	}
+};
