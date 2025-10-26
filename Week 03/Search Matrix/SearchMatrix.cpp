2a409d5d7665d961d3aa3458ae3b8a6d6c85e441
Hris <xrisko06@abv.bg>
2025-10-26 23:09:25 +0200

    Add task

---
+++ Week 03/Search Matrix/SearchMatrix.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/search-a-2d-matrix/description/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	bool searchMatrix(vector<vector<int>>& matrix, int target) {
+		int row = 0, col = matrix[0].size() - 1;
+
+		while (row < matrix.size() && col >= 0)
+		{
+			if (matrix[row][col] == target)
+				return true;
+
+			if (matrix[row][col] < target)
+				row++;
+			else
+				col--;
+		}
+
+		return false;
+	}
+};
