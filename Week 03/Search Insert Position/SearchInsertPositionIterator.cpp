e6f87da8d87b74c99382044ad1df1bf4dec61e9e
Hris <xrisko06@abv.bg>
2025-10-26 22:19:24 +0200

    Add task

---
+++ Week 03/Search Insert Position/SearchInsertPositionIterator.cpp
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/search-insert-position/description/
+#include<iostream>
+#include<vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	int searchInsert(vector<int>& nums, int target) {
+		auto it = lower_bound(nums.begin(), nums.end(), target);
+		return it - nums.begin();
+	}
+};
