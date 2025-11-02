cf49ca50e3157061660ab615124746c9ca6b15e0
Hris <xrisko06@abv.bg>
2025-11-02 12:31:01 +0200

    Add task

---
+++ Week 04/MiddleOfLinkedList/MiddleOfLinkedList.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/middle-of-the-linked-list/
+#include <iostream>
+
+struct ListNode {
+	int val;
+	ListNode* next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+	ListNode* middleNode(ListNode* head) {
+		if (!head)
+			return nullptr;
+
+		ListNode* slow = head;
+		ListNode* fast = head;
+
+		while (fast && fast->next)
+		{
+			slow = slow->next;
+			fast = fast->next->next;
+		}
+
+		return slow;
+	}
+};
