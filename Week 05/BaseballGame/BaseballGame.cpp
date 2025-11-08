04c5cccad52776e1e98fbebfb637a8f3691fed43
Hris <xrisko06@abv.bg>
2025-11-08 11:36:04 +0200

    Add task

---
+++ Week 05/BaseballGame/BaseballGame.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+	int calPoints(vector<string>& operations) {
+		stack<int> scores;
+
+		for (const string& operation : operations) {
+			if (operation == "+")
+			{
+				int top1 = scores.top(); scores.pop();
+				int top2 = scores.top();
+				scores.push(top1);
+				scores.push(top1 + top2);
+			}
+			else if (operation == "D")
+			{
+				scores.push(scores.top() * 2);
+			}
+			else if (operation == "C")
+			{
+				scores.pop();
+			}
+			else
+			{
+				scores.push(stoi(operation));
+			}
+		}
+
+		int res = 0;
+		while (!scores.empty()) {
+			res += scores.top();
+			scores.pop();
+		}
+
+		return res;
+	}
+};
