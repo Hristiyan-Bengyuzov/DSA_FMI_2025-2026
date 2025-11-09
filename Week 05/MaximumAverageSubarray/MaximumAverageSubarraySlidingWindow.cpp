abc6d4ec499b7565d8df9e3ee9bf7ba15fd96f57
Hris <xrisko06@abv.bg>
2025-11-09 13:00:31 +0200

    Add task

---
+++ Week 05/MaximumAverageSubarray/MaximumAverageSubarraySlidingWindow.cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/maximum-average-subarray-i/description/
+#include <iostream>
+#include <vector>
+#include <limits>
+
+using namespace std;
+
+// Doesn't pass all tests cause it's too slow
+// Time O(n)
+// Space O(1)
+class Solution {
+public:
+	double findMaxAverage(vector<int>& nums, int k) {
+		double sum = 0;
+		for (size_t i = 0; i < k; i++)
+			sum += nums[i];
+
+		double maxAverage = sum / k;
+
+		for (size_t i = k; i < nums.size(); i++)
+		{
+			sum += nums[i] - nums[i - k];
+			maxAverage = max(maxAverage, sum / k);
+		}
+
+		return maxAverage;
+	}
+};
