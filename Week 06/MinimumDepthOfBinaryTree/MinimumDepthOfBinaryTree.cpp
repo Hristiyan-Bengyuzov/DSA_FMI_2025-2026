4f8207ddbfe95e9d53fe6a60c078dbf2e93a2eed
Hris <xrisko06@abv.bg>
2025-11-14 17:22:22 +0200

    Add task

---
+++ Week 06/MinimumDepthOfBinaryTree/MinimumDepthOfBinaryTree.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/minimum-depth-of-binary-tree/description/
+#include <iostream>
+
+using namespace std;
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+	int minDepth(TreeNode* root) {
+		if (!root)
+			return 0;
+
+		int leftDepth = minDepth(root->left);
+		int rightDepth = minDepth(root->right);
+
+		if (!root->left)
+			return 1 + rightDepth;
+
+		if (!root->right)
+			return 1 + leftDepth;
+
+		return 1 + min(leftDepth, rightDepth);
+	}
+};
