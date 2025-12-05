82cb2fcafdc033e69228647537599459c3a3fb16
Hris <xrisko06@abv.bg>
2025-12-05 18:19:30 +0200

    Add task

---
+++ Week 07/TwoSum/TwoSum.cpp
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/two-sum/description/
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+		unordered_map<int, int> map;
+
+		for (int i = 0; i < nums.size(); i++)
+		{
+			int needed = target - nums[i];
+
+			if (map.find(needed) != map.end())
+			{
+				return { map[needed], i };
+			}
+
+			map[nums[i]] = i;
+		}
+
+		return {};
+	}
+};
