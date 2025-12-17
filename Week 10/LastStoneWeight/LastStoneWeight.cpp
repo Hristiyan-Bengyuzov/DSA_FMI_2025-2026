e0b4835c5859af69fbedaba9bff396ba9f6a6cf6
Hris <xrisko06@abv.bg>
2025-12-17 17:03:49 +0200

    Add task

---
+++ Week 10/LastStoneWeight/LastStoneWeight.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/last-stone-weight/description/
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+	int lastStoneWeight(vector<int>& stones) {
+		priority_queue<int> pq(stones.begin(), stones.end());
+
+		while (pq.size() > 1)
+		{
+			int x = pq.top(); pq.pop();
+			int y = pq.top(); pq.pop();
+
+			pq.push(x - y);
+		}
+
+		return pq.top();
+	}
+};
